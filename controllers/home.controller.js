75cc4b20e2750849d3760851218fad6f5cc8fda6
vishwasGarade <139865344+vishwasGarade@users.noreply.github.com>
2023-08-07 13:05:18 +0530

    Update home.controller.js

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,17 +1,37 @@
 const fs = require('fs');
+var flag=0;
 
 exports.getHomePage = (req, res) => {
     let query = "SELECT * FROM `players` ORDER BY id ASC"; // query database to get all the players
-
-    // execute query
-    db.query(query, (err, result) => {
+    
+    db.connect((err) => {
         if (err) {
-            res.redirect('/');
+            // throw err;
+            console.log(err);
+            res.send("Error occured during database connection!");
+            flag=0;
+            return;
         }
-
-        res.render('index.ejs', {
-            title: "Welcome to Socka | View Players",
-            players: result
-        });
+        flag=1;
+        console.log('Connected to database');
     });
+    setTimeout(()=>{
+        console.log(flag);
+        if(flag){
+            // execute query
+            db.query(query, (err, result) => {
+                if (err) {
+                    // res.send(err);
+                    res.redirect('/');
+                }
+                
+                res.render('index.ejs', {
+                    title: "Welcome to Socka | View Players",
+                    players: result
+                });
+            });
+            db.end();
+        }
+        
+    },2000)
 };
